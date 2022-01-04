b0e054f7e72a923271bbc77b580241023514c881
Graham Moucka <mouckatron@gmail.com>
2022-01-04 21:06:38 +0000

    Add flags to control rigctl-http host/port and rigctld host/port

--- cmd/rigctl-http/main.go
+++ cmd/rigctl-http/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
+	"flag"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 var rigConnection RigConnection
 
 func main() {
 
+	var myHost string
+	const myHostHelp = "Address rigctl-http will be available at"
+	var myPort int
+	const myPortHelp = "Port rigctl-http will be available at"
+	var rcHost string
+	const rcHostHelp = "Address rigctld is running at"
+	var rcPort int
+	const rcPortHelp = "Port rigctld is running on"
+
 	// get arguments
+	flag.StringVar(&myHost, "host", "localhost", myHostHelp)
+	flag.IntVar(&myPort, "port", 8080, myPortHelp)
+
+	flag.StringVar(&rcHost, "rchost", "localhost", rcHostHelp)
+	flag.IntVar(&rcPort, "rcport", 4532, rcPortHelp)
+
+	flag.Parse()
 
 	// decide if we're using hamlib or telnet (always telnet for now)
 
 	// start a RigConnection interface that implements the functions
-	rigConnection = &TelnetRigConnection{Address: "localhost:4532"}
+	rigConnection = &TelnetRigConnection{Address: fmt.Sprintf("%s:%d", rcHost, rcPort)}
 
 	rigConnection.init()
 
 	// start the API server which uses the RigConnection thing
-  router := gin.Default()
-  routerPaths(router)
-  router.Run("localhost:8080")
+	router := gin.Default()
+	routerPaths(router)
+	router.Run(fmt.Sprintf("%s:%d", myHost, myPort))
 }
